57dd2765781c568501f6717126b5649db7023c40
Sobirobert <rsobierajpsp@gmail.com>
2023-09-08 18:28:14 +0200

    Zadanie dodatkowe nr 1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] someArray = new int[7] {1,4,2,8,11,3,0};
+Console.WriteLine("Numbers before sort.");
+List<int> intList = someArray.ToList();
+
+foreach (int i in intList)
+{
+    Console.WriteLine(i);
+}
+
+List<int> sortList = intList.OrderBy(x =>x).ToList();
+
+
+Console.WriteLine("Numbers after sort.");
+foreach (int i in sortList)
+{
+    Console.WriteLine(i);
+}
