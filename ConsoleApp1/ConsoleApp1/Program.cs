1d66a10b31b2c13d8af453206cfbf9db4ab2471f
Sobirobert <rsobierajpsp@gmail.com>
2023-09-09 13:16:51 +0200

    Zadanie 6

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,28 +1,50 @@
-﻿        int[] tablica = { 64, 34, 25, 12, 22, 11, 90 };
-        Babelkowe(tablica);
-        Console.WriteLine("Posortowana tablica: ");  
+﻿using ConsoleApp1;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Xml.Linq;
 
-        foreach (int value in tablica)
-        {
-        Console.WriteLine(value);
-        }
-        
-    
+Employee employee1 = new Employee("Robert", "Sam", 23);
+Employee employee2 = new Employee("Marek", "Wick", 43);
+Employee employee3 = new Employee("John", "Snow", 33);
 
-    static void Babelkowe(int[] arr)
+employee1.AddScore(8);
+employee1.AddScore(7);
+employee1.AddScore(5);
+employee1.AddScore(8);
+employee1.AddScore(7);
+
+employee2.AddScore(1);
+employee2.AddScore(4);
+employee2.AddScore(2);
+employee2.AddScore(7);
+employee2.AddScore(4);
+
+employee3.AddScore(5);
+employee3.AddScore(1);
+employee3.AddScore(2);
+employee3.AddScore(6);
+employee3.AddScore(9);
+
+List<Employee> users = new List<Employee>()
+{
+    employee1, employee2, employee3
+};
+var maxScore = users.Max(e =>e.pointsOfEmployee);
+
+var maxScore2 = 0;
+Employee userMaxScore = null;
+
+foreach (var user in users)
+{
+    if(user.pointsOfEmployee > maxScore2)
     {
-        
-        for (int i = 0; i < arr.Length - 1; i++)
-        {
-            for (int j = 0; j < arr.Length - i - 1; j++)
-            {
-                if (arr[j] > arr[j + 1])
-                {
-                    // zamiana arr[j+1] i arr[j]
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
+        maxScore2 = user.pointsOfEmployee;
+        userMaxScore = user;
     }
+}
+
+Console.WriteLine($" Employee with max score is {userMaxScore.name}, {userMaxScore.lastName}, {userMaxScore.age} yers old, and his score is {maxScore2}");
+Console.WriteLine($" {maxScore}");
+
+
+
