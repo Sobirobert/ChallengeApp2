86fb715e4aa5551682fae6ce24abc9396ca6bf0c
Sobirobert <rsobierajpsp@gmail.com>
2023-09-21 13:46:05 +0200

    Poprawka dzień 15

--- ConsoleApp1/ZadanieZWyzwania21/Supervisor.cs
+++ ConsoleApp1/ZadanieZWyzwania21/Supervisor.cs
@@ -6,7 +6,78 @@ using System.Threading.Tasks;
 
 namespace ZadanieZWyzwania21
 {
-    internal class Supervisor
+    internal class Supervisor : IEmployee
     {
+        private List<float> grades = new List<float>();
+
+        public string name => throw new NotImplementedException();
+
+        public string surname => throw new NotImplementedException();
+
+        public void AddGrade(float grade)
+        {
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                throw new Exception("Invalid grade value");
+            }
+        }
+
+        public void AddGrade(char grade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddGrade(string grade)
+        {
+            switch (grade)
+            {
+                case "6":
+                    this.grades.Add(100);
+                    break;
+                case "5":
+                    this.grades.Add(80);
+                    break;
+                case "4":
+                    this.grades.Add(60);
+                    break;
+                case "3":
+                    this.grades.Add(40);
+                    break;
+                case "-3":
+                case "3-":
+                    this.grades.Add(35);
+                    break;
+                case "+2":
+                case "2+":
+                    this.grades.Add(25);
+                    break;
+                case "2":
+                    this.grades.Add(20);
+                    break;
+                case "1":
+                    this.grades.Add(0);
+                    break;
+                default:
+                    // this.grades.Add(0);
+                    throw new Exception("Wrong Letter");
+
+            }
+
+        }
+        public void AddGrade(double grade)
+        {
+            float number = Convert.ToSingle(grade);
+            // float number = (float)grade;
+            this.AddGrade(number);
+        }
+
+        public Statistics GetStatistics()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
