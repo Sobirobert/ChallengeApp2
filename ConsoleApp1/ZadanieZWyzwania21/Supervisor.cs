9164a67a98df6fc921da5ec15aa97be0c1c9ec5b
Sobirobert <rsobierajpsp@gmail.com>
2023-09-21 14:16:20 +0200

    15.3

--- ConsoleApp1/ZadanieZWyzwania21/Supervisor.cs
+++ ConsoleApp1/ZadanieZWyzwania21/Supervisor.cs
@@ -28,7 +28,33 @@ namespace ZadanieZWyzwania21
 
         public void AddGrade(char grade)
         {
-            throw new NotImplementedException();
+            switch (grade)
+            {
+                case 'A':
+                case 'a':
+                    this.grades.Add(100);
+                    break;
+                case 'B':
+                case 'b':
+                    this.grades.Add(80);
+                    break;
+                case 'C':
+                case 'c':
+                    this.grades.Add(60);
+                    break;
+                case 'D':
+                case 'd':
+                    this.grades.Add(40);
+                    break;
+                case 'E':
+                case 'e':
+                    this.grades.Add(20);
+                    break;
+                default:
+                    // this.grades.Add(0);
+                    throw new Exception("Wrong Letter");
+
+            }
         }
 
         public void AddGrade(string grade)
@@ -66,7 +92,7 @@ namespace ZadanieZWyzwania21
                     throw new Exception("Wrong Letter");
 
             }
-
+            
         }
         public void AddGrade(double grade)
         {
